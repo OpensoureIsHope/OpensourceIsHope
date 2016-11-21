87e9ede4d2e3667336ca9a456d6fe3cfe649f490
Hyunjin Kang <bequeller@naver.com>
2016-11-21 22:46:38 +0900

    Some comments and replace first splash image to Pikachu for test
    Signed-off-by: Hyunjin Kang <bequeller@naver.com>

--- app/src/main/java/com/semi/sopt_19th_2/SplashActivity.java
+++ app/src/main/java/com/semi/sopt_19th_2/SplashActivity.java
@@ -10,16 +10,17 @@ public class SplashActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_splash);
+        setContentView(R.layout.activity_splash);   // 이 Activity에서 어떤 xml을 보여줄건지 설정합니다.
+
 
         Handler hd = new Handler();
-        hd.postDelayed(new Runnable() {
+        hd.postDelayed(new Runnable() {             // 핸들러가 일정시간 지연뒤 post 합니다.
             @Override
             public void run() {
-                Intent intent = new Intent(getApplicationContext(),LoginActivity.class);
+                Intent intent = new Intent(getApplicationContext(),LoginActivity.class);    // 다음에 불러올 class 파일을 지정합니다.
                 startActivity(intent);
                 finish();
             }
-        }, 2000);
+        }, 2000);       // 2000의 단위를 ms로, 2초를 의미합니다.
     }
 }
